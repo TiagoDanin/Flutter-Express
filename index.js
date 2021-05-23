0ee4b6dfcba611b5d255225ead36cdf143ce219a
Tiago Danin <TiagoDanin@outlook.com>
2021-05-23 15:50:49 -0300

    feat: create server in express

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.permittedCrossDomainPolicies())
+app.use(helmet.xssFilter())
+
+app.set('trust proxy', 1)
+
+app.use('/', express.static(path.join(__dirname, 'build/web')))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
